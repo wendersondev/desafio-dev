6d5f56743297887886ce4b2d3a3e37ebb9a07675
Wenderson Silva <wendersondev@gmail.com>
2023-07-13 22:27:42 -0400

    feat - criação do projeto com arquivos iniciais

---
+++ src/main/java/com/desafio/dev/desafiodev/DesafioDevApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.dev.desafiodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/dev/desafiodev/DesafioDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.dev.desafiodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
